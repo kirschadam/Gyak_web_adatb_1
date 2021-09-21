c729a2867e6d90f5a33d25dbc3ae930d39eb50b2
kirschadam <78801902+kirschadam@users.noreply.github.com>
2021-09-21 09:13:57 +0200

    Add files via upload

---
+++ Gyak 2/Hegyek_OOP/Hegyek_OOP/Hegy.cs
@@ -0,0 +1,20 @@
+﻿namespace Hegyek_OOP
+{
+    class Hegy
+    {
+        private string hegycsucs_neve;
+        private string hegyseg;
+        private int magassag;
+
+        public string Hegycsucs_neve { get => hegycsucs_neve; set => hegycsucs_neve = value; }
+        public string Hegyseg { get => hegyseg; set => hegyseg = value; }
+        public int Magassag { get => magassag; set => magassag = value; }
+
+        public Hegy(string[] darabok)
+        {
+            hegycsucs_neve = darabok[0];
+            hegyseg = darabok[1];
+            magassag = int.Parse(darabok[2]);
+        }
+    }
+}
---
+++ Gyak 2/Hegyek_OOP/Hegyek_OOP/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Hegyek_OOP
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Hegy> hegyek = new List<Hegy>();
+
+            StreamReader reader = new StreamReader("hegyekMo.txt");
+            reader.ReadLine();
+            while (!reader.EndOfStream)
+            {
+                hegyek.Add(new Hegy(reader.ReadLine().Split(';')));
+            }
+            reader.Close();
+
+            Console.WriteLine("3. Feladat: Hegycsúcsok száma: " + hegyek.Count + " db");
+
+            float avg = 0;
+            foreach (var item in hegyek) { avg += item.Magassag; }
+            Console.WriteLine("4. Feladat: Hegycsúcsok átlagos magassága: " + (avg /= hegyek.Count) + " m");
+
+            int magasabb = 0;
+            int ind = 0;
+            for (int i = 0; i < hegyek.Count; i++)
+            {
+                if (hegyek[i].Magassag > magasabb)
+                {
+                    magasabb = hegyek[i].Magassag;
+                    ind = i;
+                }
+            }
+            Console.WriteLine("5. Feladat: A legmagasabb hegycsúcs adatai:\n\tNév: " + hegyek[ind].Hegycsucs_neve + "\n\tHegység: " + hegyek[ind].Hegyseg + "\n\tMagasság: " + hegyek[ind].Magassag + " m");
+
+            Console.Write("6.Feladat: Kérek egy magasságot: ");
+            int input = int.Parse(Console.ReadLine());
+            bool van = false;
+            foreach (var item in hegyek)
+            {
+                if(item.Magassag > input)
+                {
+                    Console.WriteLine("Van " + input + " m-nél magasabb hegycsúcs a " + item.Hegycsucs_neve);
+                    van = true;
+                    break;
+                }
+            }
+            if (!van) { Console.WriteLine("Nincs " + input + " m-nél magasabb hegycsúcs!"); }
+
+            Console.ReadKey();
+        }
+    }
+}
