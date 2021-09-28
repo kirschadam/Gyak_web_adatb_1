caf6738d78b62db58cbfe20efb7cc0833976dd6d
kirschadam <78801902+kirschadam@users.noreply.github.com>
2021-09-28 12:19:25 +0200

    Add files via upload

---
+++ Projekt1/login.php
@@ -0,0 +1,14 @@
+
+<h2>Login</h2>
+<form action="" method="post">
+    <div class="form-group">                        
+        <label for="username">Username: </label>
+        <input type="text" name="username" class="form-control" placeholder="Your username here...">
+    </div>
+    <div class="form-group">                        
+        <label for="password">Password: </label>
+        <input type="password" name="password" class="form-control" placeholder="Your password here...">
+    </div>
+    <input type="submit" class="btn btn-primary" value="Login"><br>
+    <a href="index.php?id=reglap">Register</a>
+</form>
